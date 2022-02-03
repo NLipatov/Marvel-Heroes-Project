c74a42d3bbfb0b6bb7f167580b4cfc5f83ae751b
Lipatov Nikita <lipatovnn@yahoo.com>
2022-02-03 14:39:21 +0300

    Fixed error on fetching random character

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -27,10 +27,12 @@ class RandomChar extends Component {
     }
 
     updateChar = () => {
+        console.log('updateChar called');
+        
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
-            .then(this.onCharLoaded)
+            .then(this.onCharLoaded, this.updateChar)
     }
 
     render(){
