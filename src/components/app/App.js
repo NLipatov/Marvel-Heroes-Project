94adedfe0202b11fcd7a3867137f431f5e1206cb
Lipatov Nikita <lipatovnn@yahoo.com>
2022-02-09 17:23:17 +0300

    routing via react router dom 5.3

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -17,25 +18,33 @@ const App = () => {
     }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                    <AppHeader/>
+                    <main>
+                        <Switch>
+                            <Route path="/comics">
+                                <AppBanner/>
+                                <ComicsList/>
+                            </Route>
+                            <Route path="/">
+                                <ErrorBoundary>
+                                    <RandomChar/>
+                                </ErrorBoundary>
+                                <div className="char__content">
+                                    <ErrorBoundary>
+                                        <CharList onCharSelected={onCharSelected}/>
+                                    </ErrorBoundary>
+                                    <ErrorBoundary>
+                                        <CharInfo charId={selectedChar}/>
+                                    </ErrorBoundary>
+                                </div>
+                                <img className="bg-decoration" src={decoration} alt="vision"/>
+                            </Route>
+                        </Switch>
+                    </main>
+            </div>
+        </Router>
     )
 }
 
