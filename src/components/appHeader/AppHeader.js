45af609e445d41738e1e526db2e91dea9eea2a65
Lipatov Nikita <lipatovnn@yahoo.com>
2022-02-09 18:09:35 +0300

    routing improvements, navlinks added

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,22 +1,22 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link>
+                <Link to="/">
                     <span>Marvel</span> information portal
                 </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
                     <li>
-                        <Link to="/">Characters</Link>
+                        <NavLink exact activeStyle={{"color": "#9f0013"}} to="/">Characters</NavLink>   
                     </li>
                     /
                     <li>
-                        <Link to="/comics">Comics</Link>
+                        <NavLink exact activeStyle={{"color": "#9f0013"}} to="/comics">Comics</NavLink>
                     </li>
                 </ul>
             </nav>
