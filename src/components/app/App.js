773c1808e3eadb9e95857785e5ba8d836db79689
Lipatov Nikita <lipatovnn@yahoo.com>
2022-02-10 14:13:47 +0300

    lazy load for comics page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,15 @@
+import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import AppHeader from "../appHeader/AppHeader";
-import {MainPage, ComicsPage, Page404} from '../pages';
 
+
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/spinner';
+
+const Page404 = lazy(()=> import("../pages/404"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
+
+// 661 KB
 
 const App = () => {
 
@@ -12,11 +20,13 @@ const App = () => {
             <div className="app">
                     <AppHeader/>
                     <main>
-                        <Routes>
-                            <Route path="/" element={<MainPage/>}/>
-                            <Route path="/comics/*" element={<ComicsPage/>}/>
-                            <Route path="*" element={<Page404/>}/>
-                        </Routes>
+                        <Suspense fallback={<Spinner/>}>
+                            <Routes>
+                                <Route path="/" element={<MainPage/>}/>
+                                <Route path="/comics/*" element={<ComicsPage/>}/>
+                                <Route path="*" element={<Page404/>}/>
+                            </Routes>
+                        </Suspense>
                     </main>
             </div>
         </Router>
